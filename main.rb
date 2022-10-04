54de4cfe8f189f8ceaadadd4be18e42b6b1e5d2f
goddesseyes <goddesseyes@protonmail.com>
2022-10-04 23:18:40 +0300

    test bot

--- main.rb
+++ main.rb
@@ -6,13 +6,13 @@ token = ENV['BOT_TOKEN']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    else
-      bot.api.send_message(chat_id: message.chat.id, text: 'No command')
+    if defined? message.text
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.sender_chat.title}")
+      when '/stop'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.sender_chat.title}")
+      end
     end
   end
 end
